e2b63e41a9a781ed03e395f49eb9482503e96a74
Misha <mkaravaev@gmail.com>
2017-11-20 09:24:52 +0500

    remove: Queue module and poolboy lib; implement: ratelimit on process mailbox

--- lib/ex_banking/application.ex
+++ lib/ex_banking/application.ex
@@ -7,22 +7,10 @@ defmodule ExBanking.Application do
     children = [
       ExBanking.UserSupervisor,
       {Registry, keys: :unique, name: Registry.Users},
-      :poolboy.child_spec(pool_name(), poolboy_conf(), [])
     ]
 
     opts = [strategy: :one_for_one, name: ExBanking.Supervisor]
     Supervisor.start_link(children, opts)
   end
 
-  defp poolboy_conf do
-    [{:name, {:local, pool_name()}},
-      {:worker_module, ExBanking.UserWorker},
-      {:size, 10},
-      {:max_overflow, 0}]
-
-  end
-
-  def pool_name do
-    :user_worker
-  end
 end
--- lib/ex_banking/user.ex
+++ lib/ex_banking/user.ex
@@ -1,9 +1,6 @@
 defmodule ExBanking.User do
   use GenServer
 
-  @default_demand 0
-  @init_account %{}
-
   def start_link(_, user) do
     case start_user(user) do
       {:error, {:already_started, _}} -> {:error, :user_already_exists}
@@ -11,48 +8,38 @@ defmodule ExBanking.User do
     end
   end
 
+  #SERVER
   def init(_) do
-    {:ok, {Queue.new(), @default_demand, @init_account}}
+    {:ok, %{}}
   end
 
-  def handle_call({:balance, currency}, _from, {_q, _demand, acc} = state) do
-    {:reply, {:ok, Map.get(acc, currency, 0)}, state}
+  def handle_call({:balance, currency}, _from, acc) do
+    {:reply, {:ok, Map.get(acc, currency, 0)}, acc}
   end
 
-  def handle_call({:insert, _event}, _from, {_q, demand, _acc} = state) when demand >= 10 do
-    {:reply, {:error, :too_many_requests_to_user}, state}
-  end
-  def handle_call({:insert, event}, from, {q, demand, acc}) do
-    queue =
-      Queue.insert(q, Tuple.append(event, from))
-      |> dispatch_events(demand + 1, acc)
-
-    {:noreply, {queue, demand + 1, acc}}
-  end
-
-  def handle_info({:deposit, {amount, currency}, from}, {q, demand, acc}) do
+  def handle_info({:deposit, {amount, currency}, from}, acc) do
     new_acc = add_money(acc, amount, currency)
     GenServer.reply(from, {:ok, new_acc})
 
-    {:noreply, {q, demand - 1, new_acc}}
+    {:noreply, new_acc}
   end
 
-  def handle_info({:withdraw, {withdraw_amount, currency}, from}, {q, demand, acc}) do
+  def handle_info({:withdraw, {withdraw_amount, currency}, from}, acc) do
     currency_balance = Map.get(acc, currency, 0)
 
     case extract_money(currency_balance, withdraw_amount, currency, acc) do
 
       {:ok, new_acc, new_currency_balance} ->
         GenServer.reply(from, {:ok, new_currency_balance})
-        {:noreply, {q, demand - 1, new_acc}}
+        {:noreply, new_acc}
 
       {:error, error} ->
         GenServer.reply(from, {:error, error})
-        {:noreply, {q, demand - 1, acc}}
+        {:noreply, acc}
     end
   end
 
-  def handle_info({:send, {to_pid, amount, currency}, from}, {q, demand, acc}) do
+  def handle_info({:send, {to_pid, amount, currency}, from}, acc) do
     [resp|[acc]] =
       with true <- can_withdraw?(amount, currency, acc),
            {:ok, receiver_new_balance} <- GenServer.call(to_pid, {:insert, {:deposit, {amount, currency}}}),
@@ -66,21 +53,16 @@ defmodule ExBanking.User do
 
 
     GenServer.reply(from, resp)
-    {:noreply, {q, demand - 1, acc}}
+    {:noreply, acc}
+  end
+
+  defp limit_exceed?(pid) do
+    {_, length} = :erlang.process_info(pid, :message_queue_len)
+    length < 10
   end
 
   defp can_withdraw?(amount, currency, acc) do
     Map.get(acc, currency, 0) >= amount
-  end
-
-  defp dispatch_events(queue, 0, _acc), do: queue
-  defp dispatch_events(queue, demand, acc) do
-    case Queue.pop(queue) do
-      {nil, queue}           -> queue
-      {event, updated_queue} ->
-        send(self(), event)
-        dispatch_events(updated_queue, demand - 1, acc)
-    end
   end
 
   defp start_user(name) do
